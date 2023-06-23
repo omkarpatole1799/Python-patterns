266a79485dc901b71bb6fba20046c0f059866766
Omkar Patole <omkarpatole1799@gmail.com>
2023-06-23 17:27:42 +0530

    patterns python

--- patterns.py
+++ patterns.py
@@ -176,38 +176,102 @@ print("pat 11")
 # 0 1 0 1 
 # 1 0 1 0 1
 
+def pat11(n):
+    for i in range(0, n):
+        value = 1
+        if(i % 2 == 0):
+            value = 1
+        else: 
+            value = 0
+        for i in range(0, i+1):
+            print(value, end=" ")
+            value = 1- value
+        print()
+pat11(n)
 
-for i in range(0, n):
-    start = 1
-    if(i % 2 == 0):
-        start = 1
-    else: 
-        start = 0
-    for i in range(0, i+1):
-        print(start, end=" ")
-        start = 1- start
+print("pat 12")
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+for i in range(1,n+1):
+    # upper triangle
+    for j in range(0,n-i):
+        print("", end=" ")
+    for j in range(0,i):
+        print("*", end="")
     print()
+for i in range(1,n+1):
+    #lower triangle
+    for j in range(1,i):
+        print("",end=" ")
+    for j in range(0,n-i+1):
+        print("*", end="")
+    print()
+    
 
 
+print("pat 13")
+# 1 
+# 2 3 
+# 4 5 6 
+# 7 8 9 10 
+# 11 12 13 14 15
 
+def pat13(n):
+    count = 0
+    for i in range(0,n):
+        for j in range(0,i+1):
+            count += 1
+            print(count, end=" ")
+        print()
+
+pat13(n)
+
+
+print("pat 14")
+def pat14(n):
+    for i in range(65, n+65):
+        for j in range(65, i+1):
+            print(chr(j), end="")
+        print()
+
+pat14(n)
+
+print("pat 15")
 #     *
 #    **
 #   ***
 #  ****
 # *****
-# *****
 #  ****
 #   ***
 #    **
 #     *
+def pat15(n):
+    for i in range(0,n):
+        # space
+        for j in range(0,n-i-1):
+            print(" ", end="")
+        # star
+        for j in range(0,i+1):
+            print("*", end="")
+        print()
+    for i in range(1,n):
+        #space
+        for j in range(0,i):
+            print(" ", end="")
+        #star
+        for j in range(0, n-i):
+            print("*", end="")
+        print()
 
-
-#     *
-#    **
-#   ***
-#  ****
-# *****
-#  ****
-#   ***
-#    **
-#     *
+pat15(n)
