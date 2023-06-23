a36dcf72e33fb2586340a24e9664fb8ba8ccb7b6
Omkar Patole <omkarpatole1799@gmail.com>
2023-06-23 21:30:13 +0530

    py pattern

--- patterns.py
+++ patterns.py
@@ -368,16 +368,77 @@ print("pat 20")
 # *        *
 
 # upper part
-for i in range(0,n):
+for i in range(1,n+1):
     #star 
-    for j in range(0,i+1):
+    for j in range(0,i):
         print("*", end="")
     #space
     for j in range(0,2*n-(2*i)):
-        print("1", end="")
+        print(" ", end="")
     #star
-    for j in range(0,i+1    ):
+    for j in range(0,i):
+        print("*", end="")
+    print()
+
+for i in range(1,n):
+    #star
+    for j in range(0,n-i):
         print("*", end="")
 
+    #space
+    for j in range(0,2*i):
+        print(" ", end="")
+    #star
+    for j in range(0,n-i):
+        print("*", end="")
+    
+    print()
 
+
+for i in range(1,n+1):
+    for j in range(0,i):
+        print("*", end="")
+    for j in range(0,2*n-(2*i)):
+        print(" ", end="")
+    for j in range(0,i):
+        print("*", end="")
+    print()
+for i in range(1,n):
+    for j in range(0,n-i):
+        print("*", end="")
+    for j in range(0,2*i):
+        print(" ", end="")
+    for j in range(0,n-i):
+        print("*", end="")
+    print()
+
+
+print("pat 21")
+# **********
+# ****  ****
+# ***    ***
+# **      **
+# *        *
+# *        *
+# **      **
+# ***    ***
+# ****  ****
+# **********
+
+for i in range(1,n+1):
+    for j in range(0,n-i+1):
+        print("*", end="")
+    for j in range(1, 2*i-1):
+        print(" ", end="")
+    for j in range(0,n-i+1):
+        print("*", end="")
+    print()
+
+for i in range(1,n+1):
+    for j in range(0,i):
+        print("*", end="")
+    for j in range(0,2*n-(2*i)):
+        print(" ", end="")
+    for j in range(0, i):
+        print("*", end="")
     print()
